c1bf47f52e7ee1ce9026545409ffd7b490c27322
Ataf_Georges <ataf_eleid@hotmail.com>
2023-11-08 12:26:58 +0100

    finit mon formulaire API codepostal et l'adress

--- asessts/script/script.js
+++ asessts/script/script.js
@@ -1,48 +1,135 @@
 console.log("Bien chargé mon js script !");
-//------------------------API--------------------
-let url = "https://geo.api.gouv.fr/communes?codePostal=13013&field=nom,code,codesPostaux,codeDepartement,codeRegion,population&format=son&geometry=centre";
 
- let datasAxios = await axiosTest();
-console.log("Datas type via Axios : ", datasAxios);
-async function axiosTest(){
-    const response = await axios.get("https://api-adresse.data.gouv.fr/search/?q=54+boulevard");
-    return response.data;
-}
-console.log("city ",datasAxios.features.properties);
+    
+//     let ul = document.querySelector("ul");
 
-// document.querySelector("#codePostal").addEventListener("input", function () {
-//     if (this.value.length == 5) {
-//         let url = `https://geo.api.gouv.fr/communes?codePostal=${this.value}&field=nom,code,codesPostaux,codeDepartement,codeRegion,population&format=son&geometry=centre`;
-//         fetch(url).then(response => response.json().then(data => {
-            
-//             console.log(data);
-//             let affichage = '<ul>';
-//             for (let code  of data) {
-//                 affichage += `<li>${code.nom}</li>`
-                
-//             }
-//             affichage += '<ul>';
-            
-//         })
-//         );
+
+// axiosTest().then(datasAxios=>{
+//     console.log("Data type via Axios" , datasAxios);
+
+
+
+// document.querySelector("#rue").addEventListener("input", function(){ 
+//     ul.innerHTML = ""
+//     let inputRue =  document.querySelector("#rue");
+//     inputRue = this.value.toLowerCase().split(" ").join("+");
+//     console.log("Par Rue is ", inputRue);
+
+//     for (let index = 0; index < datasAxios.features.length; index++) {
+//         let optionText = datasAxios.features[index].properties.label.toLowerCase();
+
+//         if(optionText.includes(inputRue)){
+//             let li = document.createElement("li");
+//                 li.textContent = datasAxios.features[index].properties.label;
+//                 li.style.listStyleType="none"
+//                 console.log(li.textContent);
+//                 ul.appendChild(li);   
+//         }  
 //     }
+// });
+
+// document.querySelector("#ville").addEventListener("input",function(){
+//     ul.innerHTML =""
+//     let inputVille= document.querySelector("#ville");
+//         inputVille = this.value.toLowerCase().split(" ").join("+");
+//         console.log("Par ville",inputVille);
+//     for (let index = 0; index < datasAxios.features.length; index++) {
+//         let optionText = datasAxios.features[index].properties.city.toLowerCase();
+
+//         if(optionText.includes(inputVille)){
+//             let li = document.createElement("li");
+//                 li.textContent = datasAxios.features[index].properties.city;
+//                 li.style.listStyleType="none"
+//                 console.log(li.textContent);
+//                 ul.appendChild(li);  
+//         }
+        
+//     }    
 // })
-// document.querySelector("#ville").addEventListener("input", function(){
-//     if(this.value.length == 5){
-//         fetch(url).then(response => response.json().then(data =>{
-//             console.log(data);
-//             let affichageVille = '<ul>';
-//             for (let ville of object) {
-                
-//             }
-//         }))
-//     }
+// document.querySelector("#codePostal").addEventListener("input",function(){
+//     ul.innerHTML="";
+//     let inputCodePostal = document.querySelector("#codePostal");
+//         inputCodePostal = this.value;
+//         console.log("Par Code postal ",inputCodePostal);
+//     for (let index = 0; index < datasAxios.features.length; index++) {
+//        let  optionText = datasAxios.features[index].properties.postcode;
+//         if(optionText.includes(inputCodePostal)){
+//             let li = document.createElement("li");
+//             li.textContent = datasAxios.features[index].properties.postcode;
+//             li.style.listStyleType="none"
+//             console.log(li.textContent);
+//             ul.appendChild(li);  
+//         } 
+        
+//     }    
+// })
+// }).catch(error =>{
+//     console.log("Error fetching data", error);
 // })
 
-document.querySelector("#rue").addEventListener("input",function(){
-    let recuperData = document.querySelector("#rue").value;
-    for (const adress of datasAxios) {
-        let select = document.createElement("select")
-        adress = datasAxios.city;
+// let url = "https://api-adresse.data.gouv.fr/search/?q="
+// let datasAxios = await axiosTest();
+// console.log("Datas type via Axios : ", datasAxios);
+// async function axiosTest(){
+//     const response = await axios.get(url);
+//     return response.data;
+// })
+// let datasAxios = await axiosTest();
+
+
+let urlRecherche;// url+change
+let result;
+let adressChoix= "";
+
+    let urlApi = `https://api-adresse.data.gouv.fr/search/?q=` ;
+    const datasFetch = async () => {
+        const res = await fetch(urlRecherche);
+        result = await res.json();
+        console.log("result : ", result);
     }
-})
+
+
+    let listAdress = document.querySelector("ul");
+    let inputRue = document.getElementById("rue");
+  
+   
+
+        inputRue.addEventListener("input", (event) => {
+            listAdress.innerHTML ="";
+            let inputRueValue = event.target.value;// recuprer le data de utlisateur
+            console.log(inputRueValue);
+            let  change = inputRueValue.toLowerCase().split(" ").join("+");
+             urlRecherche = urlApi + change;
+            console.log(urlRecherche);
+            if(change.length > 4){
+              datasFetch()   // appele fonction de data fetch 
+            }
+            for (let index = 0; index < result.features.length; index++) {
+                console.log("mon resulatat : ",result.features[index].properties.label);
+                let listParElement = document.createElement("li")
+                    listParElement.classList.add("list-rue");
+                    listParElement.style.listStyleType= "none"
+                    listAdress.style.cursor ="pointer"
+                    listParElement.textContent = result.features[index].properties.label;
+                    listAdress.appendChild(listParElement);
+            }
+            listAdress.addEventListener("click",(eventClick)=>{
+                    listAdress.innerHTML=""
+               adressChoix =  eventClick.target.innerText;
+               for (let index = 0; index < result.features.length; index++) {
+                if(adressChoix == result.features[index].properties.label){
+                    document.querySelector("#ville").value = result.features[index].properties.city;
+                    document.querySelector("#codePostal").value = result.features[index].properties.postcode;
+                    document.querySelector("#rue2").value = result.features[index].properties.name;
+                    document.querySelector("#ville2").value = result.features[index].properties.city;
+                    document.querySelector("#codePostal2").value = result.features[index].properties.postcode;
+                }
+                
+               }
+
+            })
+            
+          
+    })
+
+
